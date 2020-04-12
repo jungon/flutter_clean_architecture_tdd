42ebed0c6f1044341d8a2e9a5e212f5be87762d6
jungon <j017.kim@gmail.com>
2020-04-12 11:06:46 +0900

    10. Bloc Scaffolding & Input Conversion

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  @override
+  NumberTriviaState get initialState => NumberTriviaInitial();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -0,0 +1,26 @@
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaState extends Equatable {
+  NumberTriviaState([List props = const <dynamic>[]]) : super(props);
+}
+
+class NumberTriviaInitial extends NumberTriviaState {
+  @override
+  List<Object> get props => [];
+}
+
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia}) : super([trivia]);
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message}) : super([message]);
+}
